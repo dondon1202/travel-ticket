22f0b047d1285702a1ee03f099bc419e0745247c
dondon <domi61503@gmail.com>
2023-03-27 14:00:36 +0800

    改用AXIOS

--- all copy 2.js
+++ all copy 2.js
@@ -1,48 +1,18 @@
-let data = [
-  {
-    id: 0,
-    name: "肥宅心碎賞櫻3日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-    area: "高雄",
-    description: "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-    group: 87,
-    price: 1400,
-    rate: 10,
-  },
-  {
-    id: 1,
-    name: "貓空纜車雙程票",
-    imgUrl:
-      "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台北",
-    description:
-      "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-    group: 99,
-    price: 240,
-    rate: 2,
-  },
-  {
-    id: 2,
-    name: "台中谷關溫泉會1日",
-    imgUrl:
-      "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-    area: "台中",
-    description:
-      "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-    group: 20,
-    price: 1765,
-    rate: 7,
-  },
-];
-
-let obj = {};
-//抓取data資料，組好HTML結構跟字串，然後初始化
-function init() {
-  const card = document.querySelector(".ticketCard-area");
-  let str = "";
-  data.forEach(function (item, index) {
-    let content = `
+axios
+  .get(
+    "https://raw.githubusercontent.com/hexschool/js-training/main/travelAPI-lv1.json"
+  )
+  .then(function (response) {
+    // handle success
+    console.log(response.data);
+    let data = response.data;
+    let obj = {};
+    //抓取data資料，組好HTML結構跟字串，然後初始化
+    function init() {
+      const card = document.querySelector(".ticketCard-area");
+      let str = "";
+      data.forEach(function (item, index) {
+        let content = `
   <li class="ticketCard">
   <div class="ticketCard-img">
             <a href="#">
@@ -77,62 +47,62 @@ function init() {
           </div>
         </li>
     </li>`;
-    str += content;
-  });
-  card.innerHTML = str;
-}
+        str += content;
+      });
+      card.innerHTML = str;
+    }
 
-init();
+    init();
 
-//新增旅遊套票
-const travelName = document.querySelector("#ticketName");
-const travelImg = document.querySelector("#ticketImgUrl");
-const travelArea = document.querySelector("#ticketRegion");
-const travelPrice = document.querySelector("#ticketPrice");
-const travelGroup = document.querySelector("#ticketNum");
-const travelStar = document.querySelector("#ticketRate");
-const travelDescription = document.querySelector("#ticketDescription");
-const btn = document.querySelector(".addTicket-btn");
+    //新增旅遊套票
+    const travelName = document.querySelector("#ticketName");
+    const travelImg = document.querySelector("#ticketImgUrl");
+    const travelArea = document.querySelector("#ticketRegion");
+    const travelPrice = document.querySelector("#ticketPrice");
+    const travelGroup = document.querySelector("#ticketNum");
+    const travelStar = document.querySelector("#ticketRate");
+    const travelDescription = document.querySelector("#ticketDescription");
+    const btn = document.querySelector(".addTicket-btn");
 
-btn.addEventListener("click", function (e) {
-  obj.name = travelName.value;
-  obj.imgUrl = travelImg.value;
-  obj.area = travelArea.value;
-  obj.price = travelPrice.value;
-  obj.group = travelGroup.value;
-  obj.rate = travelStar.value;
-  obj.description = travelDescription.value;
-  data.push(obj);
+    btn.addEventListener("click", function (e) {
+      obj.name = travelName.value;
+      obj.imgUrl = travelImg.value;
+      obj.area = travelArea.value;
+      obj.price = travelPrice.value;
+      obj.group = travelGroup.value;
+      obj.rate = travelStar.value;
+      obj.description = travelDescription.value;
+      data.push(obj);
 
-  init();
+      init();
 
-  //清空輸入資料
-  //方法一
-  //   travelName.value = "";
-  //   travelImg.value = "";
-  //   travelArea.value = "";
-  //   travelPrice.value = "";
-  //   travelGroup.value = "";
-  //   travelStar.value = "";
-  //   travelDescription.value = "";
-  //方法二
-  const addTicket_form = document.querySelector(".addTicket-form");
-  addTicket_form.reset();
-});
+      //清空輸入資料
+      //方法一
+      //   travelName.value = "";
+      //   travelImg.value = "";
+      //   travelArea.value = "";
+      //   travelPrice.value = "";
+      //   travelGroup.value = "";
+      //   travelStar.value = "";
+      //   travelDescription.value = "";
+      //方法二
+      const addTicket_form = document.querySelector(".addTicket-form");
+      addTicket_form.reset();
+    });
 
-//地區篩選
-const regionSearch = document.querySelector(".regionSearch");
-const searchResult_text = document.querySelector("#searchResult-text");
-regionSearch.addEventListener("change", function (e) {
-  let newData = []; // 在事件處理程序開始時先宣告一個空陣列
-  let str = "";
+    //地區篩選
+    const regionSearch = document.querySelector(".regionSearch");
+    const searchResult_text = document.querySelector("#searchResult-text");
+    regionSearch.addEventListener("change", function (e) {
+      let newData = []; // 在事件處理程序開始時先宣告一個空陣列
+      let str = "";
 
-  if (e.target.value === undefined) {
-    return;
-  }
+      if (e.target.value === undefined) {
+        return;
+      }
 
-  data.forEach(function (item, index) {
-    let content = `<li class="ticketCard">
+      data.forEach(function (item, index) {
+        let content = `<li class="ticketCard">
   <div class="ticketCard-img">
             <a href="#">
               <img
@@ -166,26 +136,27 @@ regionSearch.addEventListener("change", function (e) {
           </div>
         </li>
     </li>`;
-    if (e.target.value == "") {
-      str += content;
-      console.log(newData); // 在 if 語句中訪問 newData，此時 newData 為空陣列
-      newData.push(item); // 將符合條件的元素添加到 newData 中
-    }
-    if (e.target.value == item.area) {
-      str += content;
-      console.log(newData); // 在 if 語句中訪問 newData，此時 newData 為空陣列
-      newData.push(item); // 將符合條件的元素添加到 newData 中
-    }
-  });
-  const card = document.querySelector(".ticketCard-area");
-  card.innerHTML = str;
-  searchResult_text.textContent = `本次搜尋共 ${newData.length} 筆資料`;
-});
+        if (e.target.value == "") {
+          str += content;
+          console.log(newData); // 在 if 語句中訪問 newData，此時 newData 為空陣列
+          newData.push(item); // 將符合條件的元素添加到 newData 中
+        }
+        if (e.target.value == item.area) {
+          str += content;
+          console.log(newData); // 在 if 語句中訪問 newData，此時 newData 為空陣列
+          newData.push(item); // 將符合條件的元素添加到 newData 中
+        }
+      });
+      const card = document.querySelector(".ticketCard-area");
+      card.innerHTML = str;
+      searchResult_text.textContent = `本次搜尋共 ${newData.length} 筆資料`;
+    });
 
-//新增功能 預設地區搜尋的時候，表單新增本次搜尋共 ?? 筆資料 會跟著變動
-btn.addEventListener("click", function (e) {
-  if (regionSearch.value === "地區搜尋") {
-    searchResult_text.textContent = `本次搜尋共 ${data.length} 筆資料`;
-    console.log(data);
-  }
-});
+    //新增功能 預設地區搜尋的時候，表單新增本次搜尋共 ?? 筆資料 會跟著變動
+    btn.addEventListener("click", function (e) {
+      if (regionSearch.value === "地區搜尋") {
+        searchResult_text.textContent = `本次搜尋共 ${data.length} 筆資料`;
+        console.log(data);
+      }
+    });
+  });
